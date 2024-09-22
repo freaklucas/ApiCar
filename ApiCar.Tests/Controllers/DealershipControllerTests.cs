bbb040a8b4ed7ac66887f81a084f89c109c9762f
freaklucas <lucas.oliveira.freak@gmail.com>
2024-09-21 23:54:06 -0300

    test:adicionando estrutura inicial

---
+++ ApiCar.Tests/Controllers/DealershipControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using ApiCar.Controllers;
+using ApiCar.Models;
+using ApiCar.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ApiCar.Tests.Controllers;
+
+public class DealershipControllerTests
+{
+    private readonly Mock<IDealershipService> _dealershipServiceMock;
+    private readonly DealershipController _controller;
+
+    public DealershipControllerTests()
+    {
+        _dealershipServiceMock = new Mock<IDealershipService>();
+        _controller = new DealershipController(_dealershipServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllDealerships_ShouldReturnOk_WithListOfDealerships()
+    {
+        // Arrange
+        var dealerships = new List<Dealership>
+        {
+            new Dealership { Id = 1, Name = "GWM" },
+            new Dealership { Id = 2, Name = "BMW" }
+        };
+
+        _dealershipServiceMock.Setup(service => service.GetAllDealerships()).ReturnsAsync(dealerships);
+
+        // Act
+        var result = await _controller.GetAllDealerships();
+
+        Assert.NotNull(result);
+        Assert.Equal(2, dealerships.Count);
+    }
+
+    [Fact]
+    public async Task AddDealership_ShouldReturnCreatedAtAction()
+    {
+        // arrange
+        var newDealership = new Dealership { Id = 3, Name = "Citroen"};
+        _dealershipServiceMock.Setup(service => service.CreateDealership(It.IsAny<Dealership>())).Returns(Task.CompletedTask);
+
+        // act
+        var result = await _controller.AddDealership(newDealership);
+
+        // Assert
+        var createdAtAction = Assert.IsType<CreatedAtActionResult>(result.Result);
+        var addedDealership = Assert.IsType<Dealership>(createdAtAction.Value);
+
+        Assert.Equal(3, addedDealership.Id);
+    }
+}
---
+++ ApiCar.Tests/Services/DealershipServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using ApiCar.Models;
+using ApiCar.Repositories.Interface;
+using ApiCar.Services.Impl;
+using Moq;
+using Xunit;
+
+namespace ApiCar.Tests.Services;
+
+public class DealershipServiceTests
+{
+    private readonly DealershipService _service;
+    private readonly Mock<IDealershipRepository> _dealershipRepositoryMock;
+
+    public DealershipServiceTests()
+    {
+        _dealershipRepositoryMock = new Mock<IDealershipRepository>();
+        _service = new DealershipService(_dealershipRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnAllDealerships()
+    {
+        // Arrange
+        var dealerships = new List<Dealership>
+        {
+            new Dealership
+            {
+                Id = 1,
+                Name = "Chevrolet",
+                Address = "Rua 01",
+                City = "Rio Verde",
+                Contact = "000111444",
+                CarListings = null
+            },
+            new Dealership
+            {
+                Id = 2,
+                Name = "Fiat",
+                Address = "Rua 02",
+                City = "Rio Verde",
+                Contact = "000111222",
+                CarListings = null
+            }
+        };
+
+        _dealershipRepositoryMock.Setup(repo => repo.GetAll()).ReturnsAsync(dealerships);
+
+        // Act
+        var result = await _service.GetAllDealerships();
+
+        // Assert
+        Assert.Equal(2, result.Count());
+        Assert.Contains(result, d => d.Name == "Chevrolet");
+        Assert.Contains(result, d => d.Name == "Fiat");
+    }
+
+    [Fact]
+    public async Task CreateDealership_ShouldCallRepository_WithValidDealership()
+    {
+        // Arrange
+        var dealership = new Dealership
+        {
+            Id = 1,
+            Name = "Chevrolet",
+            Address = "Rua 01",
+            City = "Rio Verde",
+            Contact = "000111444",
+            CarListings = null
+        };
+
+        // Act
+        await _service.CreateDealership(dealership);
+
+        // Assert
+        _dealershipRepositoryMock.Verify(repo => repo.Create(dealership), Times.Once);
+    }
+}
