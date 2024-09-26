76dd82fbff3eff704b7b65eac7fe2330b4ee0bc8
freaklucas <lucas.oliveira.freak@gmail.com>
2024-09-25 21:30:30 -0300

    feat:entidade testDrive

---
+++ ApiCar/Models/TestDrive.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCar.Models;
+
+public class TestDrive
+{
+    [Key] public int Id { get; set; }
+    
+    [Required] public int CarId { get; set; }
+    public virtual Car Car { get; set; }
+
+    [Required] public int DealershipId { get; set; }
+    public virtual Dealership Dealership { get; set; }
+
+    [Required] public int UserId { get; set; }
+    public virtual User User { get; set; }
+
+    [Required] public DateTime ScheduledDate { get; set; }
+
+    public string? Notes { get; set; }
+}
